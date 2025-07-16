2bfdf37f31d809b4d5d52502986f2a013ee27b16
Esteve <badia.orive@gmail.com>
2025-07-16 12:11:22 +0200

    Refactor jest environment setup to include app version from package.json

--- app/test/jest/jest.env.ts
+++ app/test/jest/jest.env.ts
@@ -1,6 +1,9 @@
 // Load environment variables from file ".env.test"
 import { config } from "dotenv"
+import packageJson from "../../package.json"
 config({ path: ".env.test" })
 
 // Fine-tune some variables specifically for jest testing:
-process.env.MOCK_ENVIRONMENT = "test";
+process.env.MOCK_ENVIRONMENT = "test"
+process.env.APP_VERSION = packageJson.version
+
