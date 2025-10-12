b9f7a8e64fab71ab8eda0279f36db8989e751d8d
Maarten Reijmerink <maarten@numblock.nl>
2025-10-12 10:16:15 +0200

    Add composables suggested by @estevebadia (wip)

---
+++ app/src/composables/useResources.ts
@@ -0,0 +1,57 @@
+import { LoadListPayload } from "src/store/resources";
+import { computed } from "vue";
+import { useStore } from "vuex";
+
+export const useResources = (type: string, options?: LoadListPayload) => {
+  const store = useStore();
+  const resources = computed(() => store.getters[`${type}/currentList`]);
+  const loadNext = async () => {
+    await store.dispatch(`${type}/loadNext`, options);
+  };
+  const hasNext = computed(() => store.getters[`${type}/hasNext`]);
+  return { resources, loadNext, hasNext };
+};
+
+// export const useResources = (
+//   type: string,
+//   options?: { [key: string]: string }
+// ) => {
+//   const resources: Ref<ResourceObject[]> = ref([]);
+//   const store = useStore();
+//   const state = computed(
+//     () => store.state[type] as ResourcesState<ResourceObject>
+//   );
+
+//   const hasNext = computed(() => store.getters[type + "/hasNext"]);
+//   console.log({ type, hasNext: hasNext.value });
+
+//   const loadNext = async (index: number, done: (stop?: boolean) => void) => {
+//     console.log(store.state[type]);
+//     console.log("loadNext", { hasNext: hasNext.value });
+//     if (hasNext.value) {
+//       await store.dispatch(type + "/loadNext", {
+//         cache: options?.cache,
+//       });
+//       // emit("page-loaded", state.value.currentPage as number);
+//     }
+//     // Stop loading if there is no next page. Note that we're not
+//     // stopping the infinite scrolling if hasNext returns undcefined.
+//     done(hasNext.value === false);
+//   };
+
+//   if (state.value.currentPage !== null) {
+//     console.log(state.value.currentPage);
+//     for (let i = 0; i <= state.value.currentPage; i++) {
+//       const page = store.getters[`${type}/page`](i);
+//       if (page) {
+//         resources.value.push(...page);
+//       }
+//     }
+//   }
+
+//   return {
+//     resources,
+//     hasNext: hasNext.value,
+//     loadNext,
+//   };
+// };
