17cb303a7a45942619a3f70d9654862f179b8e5f
Esteve <badia.orive@gmail.com>
2025-11-12 09:11:20 +0100

    Fix share button prop

--- app/src/pages/members/Member.vue
+++ app/src/pages/members/Member.vue
@@ -17,7 +17,7 @@
           flat
           round
           :title="$t('shareMember', { member: member.attributes.name })"
-          :text="member.attributes.bio"
+          :text="member.attributes.description ?? ''"
         />
         <q-btn
           v-if="canEdit"
