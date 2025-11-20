d39613b6f9a26ee63a39617fd3583c8a2cc5983a
Maarten Reijmerink <maarten@numblock.nl>
2025-11-20 14:33:23 +0100

    Add ProfileBtnMenu component, remove menu-items from MenuDrawer, fix more padding

---
+++ app/src/components/ProfileBtnMenu.vue
@@ -0,0 +1,71 @@
+<template>
+  <q-btn
+    flat
+    round
+    icon="account_circle"
+    :aria-label="$t('profile')"
+    @click.stop
+  >
+  <q-menu
+      auto-close
+      anchor="bottom right"
+      self="top right"
+    >
+      <q-list>
+        <member-header
+          id="my-member" 
+          :member="myMember" 
+          :to="`/groups/${groupCode}/members/${myMember.attributes.code}`"
+          active-class="bg-active"
+        />
+        <menu-item
+          icon="edit"
+          :title="$t('editProfile')"
+          to="/profile"
+        />
+        <menu-item  
+          icon="settings"
+          :title="$t('settings')"
+          to="/settings"
+        />
+        <menu-item
+          id="user-menu-logout"
+          icon="logout"
+          :title="$t('logout')"
+          to="/logout"
+        />
+
+        <q-separator />
+
+        <menu-item
+          icon="info"
+          :title="$t('komunitinProject')"
+          href="https://github.com/komunitin/komunitin"
+          />
+        <menu-item 
+          v-if="feedbackURL"
+          icon="feedback"
+          :title="$t('feedback')"
+          :href="feedbackURL"
+        />
+      </q-list>
+    </q-menu>
+  </q-btn>
+</template>
+<script setup lang="ts">
+import { computed } from 'vue';
+import { useStore } from 'vuex';
+import { config } from 'src/utils/config';
+
+import MemberHeader from '../components/MemberHeader.vue';
+import MenuItem from '../components/MenuItem.vue';
+
+const store = useStore();
+
+const myMember = computed(() => store.getters.myMember);
+const groupCode = computed(() => myMember?.value.group.attributes.code);
+
+const feedbackURL = config.FEEDBACK_URL;
+
+
+</script>
