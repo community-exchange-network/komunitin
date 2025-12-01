42c86d4eb5bf5d3745a3c46d74deda608d4f04ee
Esteve <badia.orive@gmail.com>
2025-12-01 17:52:03 +0100

    add mollie config vars

--- accounting/src/config.ts
+++ accounting/src/config.ts
@@ -17,10 +17,12 @@ const loadConfig = () => {
     AUTH_JWKS_URL: process.env.AUTH_JWKS_URL || "https://komunitin.org/.well-known/jwks.json",
     AUTH_JWT_ISSUER: process.env.AUTH_JWT_ISSUER || "https://komunitin.org",
     AUTH_JWT_AUDIENCE: process.env.AUTH_JWT_AUDIENCE?.split(',') || ['komunitin-app', 'komunitin-notifications'],
-    API_BASE_URL: process.env.API_BASE_URL || "https://komunitin.org/accounting",
+    API_BASE_URL: process.env.API_BASE_URL || "https://accounting.komunitin.org",
     NOTIFICATIONS_API_URL: process.env.NOTIFICATIONS_API_URL || "https://notifications.komunitin.org",
     NOTIFICATIONS_API_USERNAME: process.env.NOTIFICATIONS_API_USERNAME || "accounting",
     DOCKER: process.env.DOCKER === "true" || false,
+    APP_URL: process.env.APP_URL || "https://komunitin.org",
+    WEBHOOKS_BASE_URL: process.env.WEBHOOKS_BASE_URL || process.env.API_BASE_URL || "https://accounting.komunitin.org",
     
     // SECRETS
     // TODO: Using environment variables for secrets is a reasonable practice but not a 
@@ -29,12 +31,14 @@ const loadConfig = () => {
     MASTER_PASSWORD: process.env.MASTER_PASSWORD || undefined,
     NEW_MASTER_PASSWORD: process.env.NEW_MASTER_PASSWORD || undefined,
     NOTIFICATIONS_API_PASSWORD: process.env.NOTIFICATIONS_API_PASSWORD || undefined,
+    MOLLIE_API_KEY: process.env.MOLLIE_API_KEY || undefined,
   }
   // Remove secrets from the process.env so other libraries don't use/leak them.
   delete process.env.SPONSOR_PRIVATE_KEY
   delete process.env.MASTER_ENCRYPTION_KEY
   delete process.env.NEW_MASTER_ENCRYPTION_KEY
   delete process.env.NOTIFICATIONS_API_PASSWORD
+  delete process.env.MOLLIE_API_KEY
 
   // Validate some config
   if (!["testnet", "local", "public"].includes(config.STELLAR_NETWORK)) {
