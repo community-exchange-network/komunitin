e5fa4470155625215a76c6437caaf229e8a7fcd2
Esteve <badia.orive@gmail.com>
2026-02-16 11:15:06 +0100

    use replace instead of push in tab nav

--- app/src/pages/transactions/CreateTransaction.vue
+++ app/src/pages/transactions/CreateTransaction.vue
@@ -15,24 +15,28 @@
         <q-route-tab
           v-if="showTabs.includes('simple')"
           :to="`/groups/${code}/members/${myMemberCode}/transactions/${direction}`"
+          :replace="true"
           :label="$t('enterData')"
           icon="edit"
         />
         <q-route-tab
           v-if="showTabs.includes('qr')"
           :to="`/groups/${code}/members/${myMemberCode}/transactions/${direction}/qr`"
+          :replace="true"
           :label="$t('qrCode')"
           icon="qr_code"
         />
         <q-route-tab
           v-if="showTabs.includes('nfc')"
           :to="`/groups/${code}/members/${myMemberCode}/transactions/${direction}/nfc`"
+          :replace="true"
           :label="$t('nfcTag')"
           icon="nfc"
         />
         <q-route-tab
           v-if="showTabs.includes('multiple')"
           :to="`/groups/${code}/members/${myMemberCode}/transactions/${direction}/multiple`"
+          :replace="true"
           :label="$t('multiple')"
           icon="list"
         />
