5a10d700d4a75b3c83a1b7b2e2a42bcc0b8ac01c
Esteve <badia.orive@gmail.com>
2026-02-24 12:32:06 +0100

    Add TransactionStatusChip component and refactor account/transfer status logic

---
+++ app/src/components/TransactionStatusChip.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-chip
+    :color="color"
+    text-color="white"
+    :label="label"
+  />
+</template>
+<script setup lang="ts">
+import { useTransferStatus } from "../composables/transferStatus";
+import type { TransferState } from "../store/model";
+
+const props = defineProps<{
+  status: TransferState;
+}>();
+
+const { color, label } = useTransferStatus(() => props.status);
+</script>
