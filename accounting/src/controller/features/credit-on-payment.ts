fbf7017eb497c123ef550aac5ace6a44ade7ac2b
Esteve <badia.orive@gmail.com>
2026-03-24 11:45:49 +0100

    Wrap on-payment action inj try-catch so it does not throw unhandled rejections

--- accounting/src/controller/features/credit-on-payment.ts
+++ accounting/src/controller/features/credit-on-payment.ts
@@ -1,6 +1,7 @@
 import { FullTransfer } from "src/model";
 import { systemContext } from "src/utils/context";
 import { CurrencyPublicService, BaseService } from "..";
+import { logger } from "../../utils/logger";
 
 /**
  * Add support for updating the credit limit of an account when a payment is received,
@@ -9,26 +10,31 @@ import { CurrencyPublicService, BaseService } from "..";
  */
 export const initUpdateCreditOnPayment = (controller: BaseService) => {
   const onTransferUpdated = async (transfer: FullTransfer, currencyController: CurrencyPublicService) => {
-    // Only handle committed transfers.
-    if (transfer.state !== "committed") {
-      return
-    }
-    const ctx = systemContext()
-    const currency = await currencyController.getCurrency(ctx)
-    // Check if the currency supports this feature.
-    if (!currency.settings.defaultOnPaymentCreditLimit) {
-      return
-    }
-    // Do the job.
-    // We are interested in the destination account of the transaction.
-    const account = transfer.payee
-    const maxLimit = account.settings.onPaymentCreditLimit ?? currency.settings.defaultOnPaymentCreditLimit
-    if (account.creditLimit < maxLimit) {
-      const newLimit = Math.min(maxLimit, account.creditLimit + transfer.amount)
-      await currencyController.accounts.updateAccount(ctx, {
-        id: account.id,
-        creditLimit: newLimit
-      })
+    try {
+      // Only handle committed transfers.
+      if (transfer.state !== "committed") {
+        return
+      }
+      const ctx = systemContext()
+      const currency = await currencyController.getCurrency(ctx)
+      // Check if the currency supports this feature.
+      if (!currency.settings.defaultOnPaymentCreditLimit) {
+        return
+      }
+      // Do the job.
+      // We are interested in the destination account of the transaction.
+      const account = transfer.payee
+      const maxLimit = account.settings.onPaymentCreditLimit ?? currency.settings.defaultOnPaymentCreditLimit
+      if (account.creditLimit < maxLimit) {
+        const newLimit = Math.min(maxLimit, account.creditLimit + transfer.amount)
+        await currencyController.accounts.updateAccount(ctx, {
+          id: account.id,
+          creditLimit: newLimit
+        })
+      }
+    } catch (error) {
+      // Log the error but don't throw since this is fired asynchronously without awaiting it.
+      logger.error({ err: error, transferId: transfer.id }, 'Error updating credit on payment')
     }
   }  
 
