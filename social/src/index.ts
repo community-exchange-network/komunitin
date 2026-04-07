bfe558ff2a96904a14c90b9b8ab79db85f79f3e7
Esteve <badia.orive@gmail.com>
2026-04-07 12:45:24 +0200

    http server with express scaffolding

---
+++ social/src/index.ts
@@ -0,0 +1,21 @@
+import logger from './utils/logger'
+import { startServer } from './app'
+
+const main = async () => {
+  logger.info('Starting social service...')
+  const { stop } = startServer()
+
+  const shutdown = async (signal: string) => {
+    logger.info(`Received ${signal}, shutting down...`)
+    await stop()
+    process.exit(0)
+  }
+
+  process.on('SIGTERM', () => shutdown('SIGTERM'))
+  process.on('SIGINT', () => shutdown('SIGINT'))
+}
+
+main().catch((err) => {
+  console.error(err)
+  process.exit(1)
+})
