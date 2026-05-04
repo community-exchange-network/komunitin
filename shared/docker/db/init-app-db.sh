5b4e1561d84f1cd524109ab139265fa4cbb16ebf
Esteve <badia.orive@gmail.com>
2026-05-04 18:24:09 +0200

    remove useless line

--- shared/docker/db/init-app-db.sh
+++ shared/docker/db/init-app-db.sh
@@ -3,8 +3,6 @@
 # This line just tells the shell to stop if any error.
 set -e
 
-POSTGRES_APP_EXTENSIONS_SCHEMA="${POSTGRES_APP_EXTENSIONS_SCHEMA:-extensions}"
-
 # Create a new user that will not have the role to bypass row level security.
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
 	CREATE USER $POSTGRES_APP_USER WITH PASSWORD '$POSTGRES_APP_PASSWORD';
