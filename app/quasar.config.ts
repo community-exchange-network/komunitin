e2032f6b7605cf90ab366d243f5bd32271c59d09
Maarten Reijmerink <maarten@numblock.nl>
2026-05-07 11:36:45 +0200

    Convert sass to scss, for better editor support.

--- app/quasar.config.ts
+++ app/quasar.config.ts
@@ -33,7 +33,7 @@ export default defineConfig((ctx) => {
 
     // https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-css
     css: [
-      "app.sass",
+      "app.scss",
     ],
 
     // https://github.com/quasarframework/quasar/tree/dev/extras
