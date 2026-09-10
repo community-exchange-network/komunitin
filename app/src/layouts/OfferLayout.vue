630190b1bbbcf1342d4f1d0adbc889d831f4ff4c
Maarten Reijmerink <maarten@numblock.nl>
2026-09-10 15:01:17 +0200

    Components: OfferLayout, Carousel, Offer, OfferCard, NeedCard, Social Toolbar

--- app/src/layouts/OfferLayout.vue
+++ app/src/layouts/OfferLayout.vue
@@ -1,33 +1,39 @@
 <template>
   <div>
     <div
-      class="column q-col-gutter-md"
-      :style="$q.screen.gt.sm ? `height: ${parentHeight}px` : ''"
+      class="row q-col-gutter-md"
     >
-      <div class="col-auto item">
-        <slot name="member" />
-      </div>
-      <div
-        v-if="numImages > 0"
-        class="col-auto item relative-position"
-      >
-        <slot name="images" />
-        <div 
-          class="q-ml-lg q-mb-sm absolute-bottom-left"
+      <div class="col-md-6 col-12 column q-gutter-y-md">
+        <div>
+          <slot name="member" />
+        </div>
+        <div
+          v-if="numImages > 0"
+          class="item relative-position"
         >
+          <slot name="images" />
+          <div 
+            class="q-ml-sm q-mb-sm absolute-bottom-left"
+          >
+            <slot name="category" />
+          </div>
+        </div>
+        <div 
+          v-if="numImages === 0"
+          class="">
           <slot name="category" />
         </div>
+        <div v-if="$q.screen.gt.sm">
+          <slot name="map" />
+        </div>
       </div>
-      <div 
-        v-if="numImages === 0"
-        class="col-auto item">
-        <slot name="category" />
-      </div>
-      <div class="col-auto item sm-last">
-        <slot name="map" />
-      </div>
-      <div class="col-auto item content">
-        <slot name="content" />
+      <div class="col-md-6 col-12 column q-gutter-y-md">
+        <div class="content">
+          <slot name="content" />
+        </div>
+        <div v-if="!$q.screen.gt.sm">
+          <slot name="map" />
+        </div>
       </div>
     </div>
   </div>
@@ -41,44 +47,5 @@ export default defineComponent({
       required: true
     }
   },
-  computed: {
-    parentHeight(): number {
-      // This is the height (in pixels) of the member header.
-      let height = 88;
-      if (this.numImages > 0) {
-        // This is the height of the image (400) and its padding (16).
-        height += 416;
-        // This is the height of the thumbnails.
-        if (this.numImages > 1) {
-          height += 72 * Math.ceil(this.numImages / 4);
-        }
-      } else {
-        // This is the height of the category pill (48) and its padding (16).
-        height += 64;
-      }
-      // This is the height of the map (200), its padding (16) and its label (53).
-      height += 269;
-      return height;
-    }
-  }
 })
 </script>
-<style lang="scss" scoped>
-/* 
-On large screens, make two columns layout and send the content to the last position,
-that ends being the first element of the second column.
-*/
-@media (min-width: $breakpoint-md-min){
-  .item {
-    width: 50%;
-  }
-  .content {
-    height: 0;
-  }
-}
-@media (max-width: $breakpoint-sm-max){
-  .sm-last {
-    order: 1000;
-  }
-}
-</style>
